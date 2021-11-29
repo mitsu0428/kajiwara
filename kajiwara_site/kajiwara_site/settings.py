92fc389f9baa666a9d7d77e959338153ae0c57f2
Mitsuhiro okada <mitsuprograming@gmail.com>
2021-11-29 19:58:13 +0900

    kajiwara done

--- kajiwara_site/kajiwara_site/settings.py
+++ kajiwara_site/kajiwara_site/settings.py
@@ -128,3 +128,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#開発環境用メール設定
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#本番環境用メール設定
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'xxxxx@gmail.com'
+# EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
+# EMAIL_USE_TLS = True
