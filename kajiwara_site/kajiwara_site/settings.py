95fd44a1d22b83787be21c027e60be32761ca6d6
Mitsuhiro okada <mitsuprograming@gmail.com>
2021-11-29 23:20:59 +0900

    debug false

--- kajiwara_site/kajiwara_site/settings.py
+++ kajiwara_site/kajiwara_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vyw_h)^l_73b!lcuhfw!y+54=ot!&+nm&omkr-*0%l25clwb)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
